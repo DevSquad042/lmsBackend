1ffcc127712882fb07225bd1757a05693055df0f
Emmy the analyst <emmanuelngene145@gmail.com>
2025-08-08 12:14:20 +0100

    added more auths

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,12 +4,18 @@ import { verifyEmail } from "../controllers/auth/verifyEmail.controller.js";
 const authRouter = express.Router();
 import { login } from "../controllers/auth/login.controller.js";
 import { logout } from "../controllers/auth/logout.controller.js";
+import { forgotPassword } from "../controllers/auth/forgotPassword.controller.js";
+import { resetPassword } from "../controllers/auth/resetPassword.controller.js";
+import { googleAuth } from "../controllers/auth/goggleAuth.controller.js";
 
 
 authRouter.post("/signup", signup);
 authRouter.get("/verify-email", verifyEmail);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+authRouter.post("/forgot-password", forgotPassword);
+authRouter.post("/reset-password", resetPassword)
+authRouter.post("/goggle/callback", googleAuth);
 
 
 export default authRouter;
