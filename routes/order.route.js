b7b7b2b58afb86e8f6b2c225df5568059ffcb106
gentlemayor911-sudo <gentlemayor911@gmail.com>
2025-08-08 11:40:22 +0100

    added order controller

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { protect } from '../middlewares/auth.middlewares.js';
+import { verifyToken } from '../middlewares/verifyToken.middleware.js';
 import { placeOrder, getMyOrders } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
-router.post('/', protect, placeOrder);
-router.get('/', protect, getMyOrders);
+router.post('/', verifyToken, placeOrder);
+router.get('/', verifyToken, getMyOrders);
 
 export default router;
