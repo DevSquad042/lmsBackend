7a56a31233c91e0cf5d4203ab3e0d10b1226966e
Emmy the analyst <emmanuelngene145@gmail.com>
2025-08-09 14:57:50 +0100

    added cart controller, fixed a bug on verifyToken middleware

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import authRouter from "./routes/auth.route.js";
 import connectDB from "./config/db.js";
 import orderRoutes from "./routes/order.route.js";
 import courseRoutes from "./routes/course.route.js"
+import cartRouter from "./routes/cart.route.js";
+import cookieParser from 'cookie-parser';
+
+
 dotenv.config();
 const app = express();
 
@@ -13,9 +17,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the LMS Backend API" });
 });
+
+app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/orders', orderRoutes);
 app.use('/api/courses', courseRoutes)
+app.use('/api/cart', cartRouter);
 const PORT = process.env.PORT || 3000;
 
 connectDB();
