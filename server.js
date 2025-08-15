121c349abbc50feebb123c3e10ddb645fd899b29
Emmy the analyst <emmanuelngene145@gmail.com>
2025-08-15 12:38:44 +0100

    done

--- server.js
+++ server.js
@@ -2,19 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cookieParser from 'cookie-parser';
-import bodyParser from "body-parser";
-
-import authRouter from "./routes/auth.route.js";
-import orderRoutes from "./routes/order.route.js";
-import courseRoutes from "./routes/course.route.js";
-import cartRouter from "./routes/cart.route.js";
-import paymentRouter from "./routes/payment.route.js";
-import { paystackWebhook } from "./controllers/payment.controller.js";
-import connectDB from "./config/db.js";
-
 import reviewrouter from "./routes/reviewroute.js";
 import enrollmentRoutes from './routes/enrollment.route.js';
-
 
 
 dotenv.config();
@@ -41,10 +30,8 @@ app.use('/api/auth', authRouter);
 app.use('/api/orders', orderRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/cart', cartRouter);
-app.use('/api/payment', paymentRouter);
 app.use('/api/review', reviewrouter);
 app.use('/api/enrollments', enrollmentRoutes);
-
 const PORT = process.env.PORT;
 
 // --- Connect DB and start server ---
