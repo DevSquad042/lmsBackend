cd41c2e34cff7e55fc0c6b1561171a1a53f82c99
Emmy the analyst <emmanuelngene145@gmail.com>
2025-08-29 13:43:24 +0100

    protected get all instructor routes

--- routes/instructors.route.js
+++ routes/instructors.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAllInstructors } from "../controllers/instructor.controller.js";
+import { verifyToken } from "../middlewares/verifyToken.middleware.js";
 
 
 const instructorsRoute = express.Router();
-
+instructorsRoute.use(verifyToken)
 instructorsRoute.get("/", getAllInstructors);
 export default instructorsRoute;
