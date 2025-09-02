1679060c4f3102b8ac2d6c3d6bf2888dbd93a387
Emmy the analyst <emmanuelngene145@gmail.com>
2025-09-02 14:37:31 +0100

    removed verify token middleware from instructors routes

--- routes/instructors.route.js
+++ routes/instructors.route.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { getAllInstructors } from "../controllers/instructor.controller.js";
-import { verifyToken } from "../middlewares/verifyToken.middleware.js";
 
 
 const instructorsRoute = express.Router();
-instructorsRoute.use(verifyToken)
 instructorsRoute.get("/", getAllInstructors);
 export default instructorsRoute;
