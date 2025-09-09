cd0e5c6fffc15dde54bed7ada83e62165b02c937
frankly-Droid <franklydemand@gmail.com>
2025-09-09 04:27:54 +0100

    fix

--- controllers/instructor.controller.js
+++ controllers/instructor.controller.js
@@ -44,15 +44,6 @@ import Course from "../models/course.model.js"
     res.status(500).json({ message: "Server error" });
   }
 };
-
-    try {
-        
-        const instructors = await User.find({role: 'instructor'});
-        res.status(200).json(instructors);
-    } catch (error) {
-        console.error("Error fetching instructors:", error);
-        res.status(500).json({ message: "Server error" });
-    }
   
 
 export const getCoursesByInstructorId = async (req, res) => {
